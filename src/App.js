d770b4c237ce189f75b9f7920e91135bee75bf64
aksaakov <a.cealicov@gmail.com>
2021-06-11 22:07:54 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import Container from '@material-ui/core/Container';
+
+function App() {
+ 
+  useEffect(()=>{
+  
+  }, [])
+ 
+  return (
+    <React.Fragment>
+      <Container maxWidth="md" className="container"> 
+       hello world
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
